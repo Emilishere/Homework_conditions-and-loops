08e037cb3b2e4e70436b67c7a63a3097dfcf9dd8
Emil Huseynli <emil.huseynli.std@bhos.edu.az>
2022-05-21 10:26:40 +0400

    first commit

---
+++ Lab1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        #region Task1
+        //{
+        //    //Task 1
+        //    Console.WriteLine("baslangic ededi daxil et:");
+        //    int from = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("son ededi daxil et");
+        //    int to = Convert.ToInt32(Console.ReadLine());
+        //    int sum = 0;
+        //    for (; from <= to; from++)
+        //    {
+        //        sum = sum + from;
+        //    }
+        //    Console.WriteLine(sum);
+        //}
+        #endregion
+        #region task2
+        //{
+        //    Console.WriteLine("baslangic ededi daxil et:");
+        //    int from = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("son ededi daxil et");
+        //    int to = Convert.ToInt32(Console.ReadLine());
+        //    float sum = 0;
+        //    int count = 0;
+        //    float avg = 0;
+        //    for (; from <= to; from++)
+        //    {
+        //        sum = sum + from;
+        //        count++;
+        //    }
+        //    avg = sum / count;
+        //    Console.WriteLine(avg);
+        //}
+        #endregion
+        #region Task4
+        //{
+        //    Console.WriteLine("baslangic ededi daxil et:");
+        //    int from = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("son ededi daxil et");
+        //    int to = Convert.ToInt32(Console.ReadLine());
+        //    int sum = 0;
+        //    if (from % 2 == 0)
+        //    {
+        //        from = from + 1;
+        //    }
+        //    for (; from <= to; from = from + 2)
+        //    {
+        //        sum = sum + from;
+        //    }
+        //    Console.WriteLine(sum);
+        //}
+        #endregion
+        #region Task3
+        //{
+        //    bool isContinue = true;
+        //    do
+        //    {
+        //        Console.WriteLine("Enter your age");
+        //        byte a = Convert.ToByte(Console.ReadLine());
+        //        if (a < 18)
+        //        {
+        //            Console.WriteLine("Your age is out of range");
+        //            isContinue = true;
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Enter your score");
+        //            int b = int.Parse(Console.ReadLine());
+        //            if (b < 50)
+        //            {
+        //                Console.WriteLine("Your score is out of range");
+        //                isContinue = true;
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Congrats, registration completed success");
+        //            }
+        //        }
+
+        //    } while (isContinue);
+        //}
+        #endregion
+    }
+}
